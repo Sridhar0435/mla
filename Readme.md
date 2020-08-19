58b402866fcbd42636ed68c4485e99f5dbbbbd4d
Sridhar0435 <ssri668@gmail.com>
2020-08-19 21:26:20 +0530

    initial commit

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 npm install express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
 npm install -D nodemon concurrently
 concurrently -- allows to intract the react and node can run at the same time with front and back end
+
+//Posts folder for posts and likes, comments
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Connect DB
+connectDB();
+
+//INIT middleware
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("API running"));
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//Define Routes
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
